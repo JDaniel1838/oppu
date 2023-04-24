ca58a6ffe4d3f9744760877d26f051db9249496c
Jose Cristian <torres.jesus.176@gmail.com>
2023-04-23 22:45:40 -0600

    Cambios en la interfaz de usuario

--- oppu/core/urls.py
+++ oppu/core/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import HomePageView
+from . import views
 
 urlpatterns = [
     path('',HomePageView.as_view(),name="home"),
+    path('planes/',views.planes,name="planes"),
+    path('contact/',views.contact,name="contact"),
 ]
 
 #path('about/',views.about,name="about"),
--- oppu/core/views.py
+++ oppu/core/views.py
@@ -5,4 +5,8 @@ from django.views.generic.base import TemplateView
 class HomePageView(TemplateView):
     template_name = "core/home.html"
 
+def planes(request):
+    return render(request, "core/planes.html")
 
+def contact(request):
+    return render(request, "core/contact.html")
